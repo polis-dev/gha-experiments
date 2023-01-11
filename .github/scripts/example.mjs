02bc570abfb5268adfb0159a1a6c41981851763b
Jake Logemann <820715+jakelogemann@users.noreply.github.com>
2023-01-11 09:55:02 -0500

    playing w/ scripts

--- .github/scripts/example.mjs
+++ .github/scripts/example.mjs
@@ -1,3 +1,23 @@
+import { exec } from '@actions/exec';
+
+export async function exampleExec() {
+  let myOutput = '';
+  let myError = '';
+
+  const options = {};
+  options.listeners = {
+    stdout: (data) => {
+      myOutput += data.toString();
+    },
+    stderr: (data) => {
+      myError += data.toString();
+    }
+  };
+  options.cwd = './lib';
+
+  await exec.exec('echo', ['node', 'index.js', 'foo=bar'], options);
+}
+
 export default function printStuff() {
   console.log('stuff')
 }
