570814cd2f17d4d42e70c11cd8708e01ff9a3039
Jake Logemann <820715+jakelogemann@users.noreply.github.com>
2023-01-11 10:09:46 -0500

    playing w/ scripts

--- .github/scripts/example.mjs
+++ .github/scripts/example.mjs
@@ -17,6 +17,25 @@ export async function exampleExec({ exec }) {
   await exec.exec('echo', ['node', 'index.js', 'foo=bar'], options);
 }
 
+export async function exampleGraphQL({ github, context }) {
+  const query = `query($owner:String!, $name:String!, $label:String!) {
+              repository(owner:$owner, name:$name){
+                issues(first:100, labels: [$label]) {
+                  nodes {
+                    id
+                  }
+                }
+              }
+            }`;
+  const variables = {
+    owner: context.repo.owner,
+    name: context.repo.repo,
+    label: 'wontfix'
+  }
+  const result = await github.graphql(query, variables)
+  console.log("::group::GraphQL Query\n", result, "\n::endgroup::")
+}
+
 export function printStuff() {
   console.log('stuff')
 }
